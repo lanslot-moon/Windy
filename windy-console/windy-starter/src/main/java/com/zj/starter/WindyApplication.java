0956acdd76e718ae1059153c4a1ba6fb27ce0464
guyuelan <1561107002@qq.com>
2025-03-20 16:40:11 +0800

    添加定时功能

--- windy-console/windy-starter/src/main/java/com/zj/starter/WindyApplication.java
+++ windy-console/windy-starter/src/main/java/com/zj/starter/WindyApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author guyuelan
  * @since 2023/1/28
  */
+@EnableScheduling
 @ComponentScan("com.zj")
 @MapperScan("com.zj.*.mapper")
 @SpringBootApplication
